27f43bff5eb2506c108cf02e409d2780633b2225
Alexrubiks <alexrubiks@gmail.com>
2026-07-14 17:22:51 +0200

    correction url

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,4 +1,4 @@
-const API_BASE = `${import.meta.env.VITE_API_URL}/api/`;
+const API_BASE = `${import.meta.env.VITE_API_URL}`;
 
 export const API_URLS = {
   guessCubeur: `${API_BASE}guess/cubeur/`,
