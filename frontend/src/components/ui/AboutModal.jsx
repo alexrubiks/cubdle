f25e7e2003072e942f05233487425bb7fa40ecb7
Alexrubiks <alexrubiks@gmail.com>
2026-07-31 19:16:27 +0200

    1.3.2

--- frontend/src/components/ui/AboutModal.jsx
+++ frontend/src/components/ui/AboutModal.jsx
@@ -113,7 +113,7 @@ export default function AboutModal({ onClose }) {
         {/* SIGNATURE */}
         <div className="text-center pt-2">
           <p className="text-xs text-black/40 mt-1">
-            Cubdle 1.3.1 · Alexis Tremellat
+            Cubdle 1.3.2 · Alexis Tremellat
           </p>
         </div>
 
